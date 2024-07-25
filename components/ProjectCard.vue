96344f0ce69165819ff271fb7b9fae16e5604923
Patrick Parker <patrickjpark@gmail.com>
2024-07-25 15:34:45 -0600

    Basic cards

---
+++ components/ProjectCard.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="flex p-4 border border-gray-700 bg-white drop-shadow-md m-4">
+        <div class="pr-4">
+            <p class="bg-blue-600 w-32 h-32">Placeholder for the image</p>
+        </div>
+        <div>
+            <p>{{ props.title }}</p>
+            <p>{{ props.description }}</p>
+            <p>Tech used: 
+                <span v-for="tech, i in props.tech" :key="i">
+                    {{ tech }}{{ props.tech.length - 1 !== i ? ', ' : ''}}
+                </span>
+            </p>
+            <a :href="props.codeLink">Code</a>
+            <a v-if="props.demoLink" :href="props.demoLink">Demo</a>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    tech: {
+        type: Array,
+        required: true
+    },
+    codeLink: {
+        type: String,
+        required: true
+    },
+    demoLink: {
+        type: String,
+        required: false
+    }
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,34 @@
 <template>
   <div>
     <p>Hello to all</p>
-    <TestingGround />
+    <div class="flex flex-col justify-center items-center">
+      <ProjectCard
+        v-for="project in projects"
+        :key="project.title"
+        :title="project.title"
+        :description="project.description"
+        :tech="project.tech"
+        :codeLink="project.codeLink"
+        :demoLink="project.demoLink"
+      />
+    </div>
   </div>
 </template>
+
+<script setup>
+const projects = ref([
+  {
+    title: 'First Project',
+    description: 'This is the sd',
+    tech: ['werw', 'TypeScript'],
+    codeLink: 'sdfsdfsdf'
+  },
+  {
+    title: 'Second Project',
+    description: 'This is the sd',
+    tech: ['werw', 'TypeScript'],
+    codeLink: 'sdfsdfsdf',
+    demoLink: 'sdfsdfsdf'
+  }
+]);
+</script>

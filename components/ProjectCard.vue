33bb37c70fa60b07c7feb7ca57c820237131dbe8
Patrick Parker <patrickjpark@gmail.com>
2024-07-26 16:39:52 -0600

    Create a card component to present projects

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,18 +1,34 @@
 <template>
-    <div class="flex p-4 border border-gray-700 bg-white drop-shadow-md m-4">
+    <div class="flex flex-col sm:flex-row items-center sm:items-stretch m-4 w-4/5 sm:w-2/3 max-w-xl p-4 rounded-lg border border-gray-700 bg-white drop-shadow-md">
         <div class="pr-4">
-            <p class="bg-blue-600 w-32 h-32">Placeholder for the image</p>
+            <p class="bg-blue-600 w-40 h-40">Placeholder for the image</p>
         </div>
-        <div>
-            <p>{{ props.title }}</p>
-            <p>{{ props.description }}</p>
+        <div class="flex flex-col justify-between items-center md:items-baseline w-full">
+            <div class="flex justify-between items-baseline w-full">
+                <p class="font-bold text-lg">{{ props.title }}</p>
+                <div class="hidden md:block ml-1">
+                    <n-button class="mr-2 mb-1 w-16" tertiary tag="a" :href="props.codeLink">
+                        Code
+                    </n-button>
+                    <n-button class="w-16" type="info" tag="a" v-if="props.demoLink" :href="props.demoLink">
+                        Demo
+                    </n-button>
+                </div>
+            </div>
+            <p class="mb-1">{{ props.description }}</p>
             <p>Tech used: 
-                <span v-for="tech, i in props.tech" :key="i">
-                    {{ tech }}{{ props.tech.length - 1 !== i ? ', ' : ''}}
-                </span>
+                <n-tag class="mr-1" v-for="tech, i in props.tech" :key="i">
+                    {{ tech }}
+                </n-tag>
             </p>
-            <a :href="props.codeLink">Code</a>
-            <a v-if="props.demoLink" :href="props.demoLink">Demo</a>
+            <div class="block md:hidden mt-3">
+                <n-button class="mr-2" tertiary tag="a" :href="props.codeLink">
+                    Code
+                </n-button>
+                <n-button type="info" tag="a" v-if="props.demoLink" :href="props.demoLink">
+                    Demo
+                </n-button>
+            </div>
         </div>
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -18,17 +18,16 @@
 <script setup>
 const projects = ref([
   {
-    title: 'First Project',
-    description: 'This is the sd',
-    tech: ['werw', 'TypeScript'],
-    codeLink: 'sdfsdfsdf'
+    title: 'VeggieTable',
+    description: 'Resource to help new cooks understand the best way to store their produce.',
+    tech: ['Vue', 'Nuxt', 'TypeScript'],
+    codeLink: 'https://github.com/patrickjparker/VeggieTable',
   },
   {
-    title: 'Second Project',
-    description: 'This is the sd',
-    tech: ['werw', 'TypeScript'],
-    codeLink: 'sdfsdfsdf',
-    demoLink: 'sdfsdfsdf'
+    title: 'Review Analysis Pipeline Tester',
+    description: 'As part of a larger project, I needed to test different ways of analyzing customer reviews across a specific type of product. This tool allows me to compare the results of different analysis methods for any type of product.',
+    tech: ['PocketBase', 'Redis Queues', 'Nuxt', 'Python', 'Golang'],
+    codeLink: 'https://github.com/patrickjparker/pipeline-tester',
   }
 ]);
 </script>

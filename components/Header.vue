f1504a5bec5204f8e64f054dcce71fa547872985
Patrick Parker <patrickjpark@gmail.com>
2024-08-07 15:46:23 -0600

    Make header more responsive

--- components/Header.vue
+++ components/Header.vue
@@ -5,15 +5,13 @@
             <h1 class="text-2xl font-bold">My Portfolio</h1>
             </div>
         </div>
-        <div class="h-72">
-            <WaveHeader>
-                <div class="flex relative justify-center items-center h-full z-10">
-                    <img src="/photos/portrait.jpg" alt="Profile Picture" class="rounded-full h-48 w-48 m-4">
-                    <p class="text-white font-semibold sm:w-1/2 p-8 text-lg">
-                        Hi, I'm Patrick. I'm a software engineer with a passion for solving problems. I'm currently working on a few projects to help me learn new technologies and improve my skills. I'm always looking for new opportunities to learn and grow as a developer.
-                    </p>
-                </div>
-            </WaveHeader>
-        </div>
+        <WaveHeader>
+            <div class="py-12 flex relative justify-center items-center flex-col md:flex-row h-full z-10">
+                <img src="/photos/portrait.jpg" alt="Profile Picture" class="rounded-full h-48 w-48 m-4">
+                <p class="text-white font-semibold sm:w-1/2 p-8 text-lg">
+                    Hi, I'm Patrick. I'm a software engineer with a passion for solving problems. I'm currently working on a few projects to help me learn new technologies and improve my skills. I'm always looking for new opportunities to learn and grow as a developer.
+                </p>
+            </div>
+        </WaveHeader>
     </div>
 </template>
